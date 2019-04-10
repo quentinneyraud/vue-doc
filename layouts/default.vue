1db9fb39a4998ce7cee33cea02cc7f8c3aab7d43
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-04-10 14:16:00 +0200

    update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,35 @@
 <template>
-  <div>
-    <nuxt/>
+  <div id="root">
+    <span class="bpDebug">
+      breakpoint: {{ bp }}
+    </span>
+    <nuxt />
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import { mapState } from 'vuex'
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
+export default {
+  computed: {
+    ...mapState({
+      bp: state => state.window.breakpoint
+    })
+  },
+  mounted () {
+    window.addEventListener('resize', () => {
+      this.$store.dispatch('window/DETECT_BREAKPOINTS')
+    })
+    this.$store.dispatch('window/DETECT_ALL')
+  }
 }
+</script>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins'
+.bpDebug
+  position fixed
+  top 0
+  left 0
+  font()
 </style>
-
